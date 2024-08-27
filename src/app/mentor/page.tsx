0eb6c3338309ec54ad4892e44e8730bfde2ccb23
Hawker <nyabentley@gmail.com>
2024-08-26 21:47:16 -0400

    Add link to mentor page

---
+++ src/app/mentor/page.tsx
@@ -0,0 +1,11 @@
+//where to view all the mentors
+//can route to the create mentor page
+const Mentor = () => {
+    return (
+        <main>
+            <h1>Mentor Page</h1>
+        </main>
+    );
+}
+
+export default Mentor;
---
+++ src/app/pages/CreateMentor.tsx
@@ -0,0 +1,9 @@
+const CreateMentor = () => {
+    return (
+        <div>
+            <h1>Create Mentor</h1>
+        </div>
+    );
+};
+
+export default CreateMentor;
