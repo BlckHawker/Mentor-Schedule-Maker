0c7a8e68aba0ed30090130acf7dfb817dc15ade3
Hawker <nyabentley@gmail.com>
2024-10-21 17:24:58 -0400

    Add css to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@ import NavBar from '@/components/NavBar';
 export default function Home() {
   return (
       <div>
-        <h1>Home Page</h1>
         <NavBar/>
-        <h3>What is this?</h3>
-        <p>This website displays all the possible schedules for mentors given their availability.</p>
+        <h2 style={{textAlign: "center"}}>What is this?</h2>
+        <p style={{textAlign: "center"}}>This website displays all the possible schedules for mentors given their availability. Here is a list of what all the pages do.</p>
+
+        <div style={{textAlign: "center"}}>
+          <p><b>Create/Edit Mentor</b>: Add a mentor into local storage, or change an existing mentor's availability</p>
+          <p><b>View Mentors</b>: View all mentors in local storage. Delete a mentor from local storage</p>
+          <p><b>Generate Schedules</b>: Generate schedules based on a variety of parameters</p>
+          <p><b>View Schedules</b>: View generated schedules in local storage</p>
+        </div>
+        
       </div>
   );
 }
